6c30de3ccacc86353d3d8130ea7c04580427e19c
Patrick Jones <pmichaeljones@gmail.com>
2014-12-23 17:51:59 -0600

    added js and css files" ;

--- twilio_vocab_app.rb
+++ twilio_vocab_app.rb
@@ -3,28 +3,26 @@ require "sinatra/activerecord"
 require 'sinatra/base'
 require 'sinatra/assetpack'
 
-# class App < Sinatra::Base
-#   register Sinatra::AssetPack
-#   assets do
-#     js :application, [
-#       '/js/jquery.js',
-#       '/js/app.js'
-#       # You can also do this: 'js/*.js'
-#     ]
+class App < Sinatra::Base
+  register Sinatra::AssetPack
+  assets do
+    js :application, [
+      'js/*.js'
+    ]
 
-#     css :application, [
-#       '/css/jqueryui.css',
-#       '/css/reset.css',
-#       '/css/foundation.sass',
-#       '/css/app.sass'
-#      ]
+    css :application, [
+      '/css/foundation.css',
+      '/css/foundation.min.css',
+      '/css/normalize.css',
+     ]
 
-#     js_compression :jsmin
-#     css_compression :sass
-#   end
-
-# end
+    js_compression :jsmin
+    css_compression :sass
+  end
 
   get '/' do
     haml :index
   end
+
+end
+
