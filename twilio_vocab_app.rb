1fabc33315edf987cb23ef9bdad8aa26958b9d8d
Patrick Jones <pmichaeljones@gmail.com>
2014-12-23 17:35:22 -0600

    added css and js folders and foundation assets

--- twilio_vocab_app.rb
+++ twilio_vocab_app.rb
@@ -1,7 +1,30 @@
 require "sinatra"
 require "sinatra/activerecord"
+require 'sinatra/base'
+require 'sinatra/assetpack'
 
-get '/' do
-  haml :index
-end
+# class App < Sinatra::Base
+#   register Sinatra::AssetPack
+#   assets do
+#     js :application, [
+#       '/js/jquery.js',
+#       '/js/app.js'
+#       # You can also do this: 'js/*.js'
+#     ]
 
+#     css :application, [
+#       '/css/jqueryui.css',
+#       '/css/reset.css',
+#       '/css/foundation.sass',
+#       '/css/app.sass'
+#      ]
+
+#     js_compression :jsmin
+#     css_compression :sass
+#   end
+
+# end
+
+  get '/' do
+    haml :index
+  end
