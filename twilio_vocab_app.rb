586e25ca2a7b1d45f96cc2d2553d5aa2e04aa304
Patrick Jones <pmichaeljones@gmail.com>
2014-12-23 18:01:40 -0600

    created public folder for css ns JS

--- twilio_vocab_app.rb
+++ twilio_vocab_app.rb
@@ -1,28 +1,8 @@
 require "sinatra"
 require "sinatra/activerecord"
-require 'sinatra/base'
-require 'sinatra/assetpack'
 
-class App < Sinatra::Base
-  register Sinatra::AssetPack
-  assets do
-    js :application, [
-      'js/*.js'
-    ]
-
-    css :application, [
-      '/css/foundation.css',
-      '/css/foundation.min.css',
-      '/css/normalize.css',
-     ]
-
-    js_compression :jsmin
-    css_compression :sass
-  end
-
-  get '/' do
-    haml :index
-  end
-
+get '/' do
+  haml :index
 end
+
 
