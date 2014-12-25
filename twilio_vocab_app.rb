aa61a6c44d96c8fb04e27c7ac856264daf5e6258
Patrick Jones <pmichaeljones@gmail.com>
2014-12-24 23:15:56 -0600

    added base spec file

--- twilio_vocab_app.rb
+++ twilio_vocab_app.rb
@@ -1,13 +1,16 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "pry"
 require_relative "models/user.rb"
+
 
 get '/' do
   haml :index
 end
 
-get '/:number' do
+get '/dog/:number' do
   haml :user_page
+  render :
 end
 
 
