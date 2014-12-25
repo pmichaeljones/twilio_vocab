3d26d7733d0d681772f41720ccd3a04c7c9647b9
Patrick Jones <pmichaeljones@gmail.com>
2014-12-25 00:28:20 -0600

    added respec gem and initiated tests

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,3 +1,4 @@
+#require "rspec"
 require_relative '../models/definition.rb'
 
 describe Definition do
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,8 @@
-require_relative '../models/user.rb'
+require 'rspec'
 
 describe User do
 
   it {should have_many(:definitions) }
 
 end
+
