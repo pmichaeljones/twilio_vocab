5cad6e3646b7d304bdd54e6016b3f9c4a422d094
Patrick Jones <pmichaeljones@gmail.com>
2014-12-27 20:27:13 -0600

    added session cookies

--- twilio_vocab_app.rb
+++ twilio_vocab_app.rb
@@ -5,19 +5,27 @@ require "twilio-ruby"
 require_relative "models/user.rb"
 require_relative "models/definition.rb"
 
+enable :sessions
 
 get '/' do
   haml :index
 end
 
 get '/incoming' do
+  session["known_user"] ||= false
   #binding.pry
   if params[:Body].downcase == "vocab"
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message "We don't recognize your number. Want to set up an account? It takes one minute, no joke."
     end
+    session["known_user"] == true
     twiml.text
   else
+    binding.pry
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Back for more I see."
+    end
+    twiml.text
   end
 end
 
