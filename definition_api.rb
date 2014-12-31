23f9e70e7bea8b5b033079764b0af70d4dcc97f6
Patrick Jones <pmichaeljones@gmail.com>
2014-12-30 20:17:00 -0700

    removed unneeded method in DefintionAPI module

--- definition_api.rb
+++ definition_api.rb
@@ -6,10 +6,6 @@ module DefinitionAPI
 
   class Definition
 
-    def self.look_up(word)
-      binding.pry
-    end
-
     # Makes call to API definition and returns the first definition in string format
     def self.get_definition(word)
       uri = URI("http://www.stands4.com/services/v2/defs.php?uid=3692&tokenid=#{ENV["DEFINITION_API_TOKEN"]}&word=#{word}")
